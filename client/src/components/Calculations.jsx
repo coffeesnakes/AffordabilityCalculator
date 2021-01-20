552381265024c113167c95265675926fa46f2ce6
Jacob Berg <coffeesnakes@gmail.com>
2021-01-19 16:35:18 -0800

    pseudocoded understanding of calculations needed

---
+++ client/src/components/Calculations.jsx
@@ -0,0 +1,23 @@
+
+const Calculations () {
+
+  /*
+home price will effect principle, payment, taxes, and downpayment
+
+30 year rate payment = 0.005%
+
+interest rate effects principle and payment total only
+
+downpayment effects payment and principle and mortgage insurance
+
+principle === .04 of payment
+
+taxes === .14 of payment
+
+home insurance is $75 flat always
+
+mortgage insurance === 0.07 of payment, only if downpayment is less than 20%
+
+
+  */
+}
