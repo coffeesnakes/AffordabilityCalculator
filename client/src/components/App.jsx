14631262f6e5f48a248c223755ea4704b9b50af2
Jacob Berg <coffeesnakes@gmail.com>
2021-01-19 16:50:37 -0800

    refactor state to fit incoming calculations that will make state reflect sliders and inputs

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Header from './Header';
 import Display from './Display';
 import Controls from './Controls';
+import * as calc from './utils/calculator';
 
 const AppContainer = styled.div`
 width: 100%;
@@ -16,16 +17,23 @@ class App extends Component {
     super(props);
     this.state = {
       loanType: '30-year fixed',
-      homePrice: 1500000,
-      payment: 10000,
-      downPayment: 750000,
+      homePrice: null,
+      payment: null,
+      downPayment: null,
       percentDown: 0.2,
       interestRate: 2.94,
-      principle: 11363,
-      propertyTaxes: 1847,
+      principle: null,
+      propertyTaxes: null,
       homeInsurance: 75,
-      mortgageInsurace: 0,
+      mortgageIns: 0,
+      loading: true,
+      error: null,
     };
+    this.handlePriceChange = this.handlePriceChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.handlePriceChange(1400000);
   }
 
   render() {
